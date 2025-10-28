dd7c86266148f4345a89a243b74d3c2a5fe15d8d
dohyun <aofhoa@gmail.com>
2025-10-28 21:38:56 +0900

    feat: 포인터

--- chapter4_02.c
+++ chapter4_02.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
 
 void chapter4_02(void) {
-//  의사결정문
+  // Pointer in Mamory
+
+  int a = 0x12345678;
+  int *ptr;
+
+  ptr = &a;
+
+  // &ptr => 0x2004;
+  // &&ptr => x
+  // (short *)ptr => 0x1234;
+  // *(short *)ptr => 0x1234;
+
 }
